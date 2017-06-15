13547ffc85e545138a626bbcc5f263966f9610ee
Akira Youngblood <akirayoungblood@me.com>
2017-06-14 21:50:21 -0600

    Adding two tests that exercise space set/get

--- unit-test/space-test.c
+++ unit-test/space-test.c
@@ -32,14 +32,76 @@ static const char *test_space_create_destroy() {
     return 0;
 }
 
-static const char *test_space_set_get_small() {
+static const char *test_space_set_get_large() {
+    static const uint64_t LARGE = (1<<14);
+    cell_t cells[LARGE];
+    cell_t cell;
+    vector3_t vectors[LARGE];
+    space_t *space = NULL;
+    uint64_t i;
+    srand((unsigned int)time(NULL));
+
+    space = space_create(vector3(0x100,0x100,4),LARGE>>4);
+    // Set lots of cells all over the place
+    // This tests (primarily) the hash access
+    for (i = 0; i < LARGE; ++i) {
+        cells[i].i = rand();
+        // don't randomize x so vectors are unique
+        vectors[i] = vector3((signed long long)i - (signed long long)(LARGE>>1),rand(),rand());
+        space_set(space, vectors[i], cells[i]);
+    }
+    // Check cells
+    for (i = 0; i < LARGE; ++i) {
+        cell = space_get(space, vectors[i]);
+        mu_assert(_FL "bad cell value", cell.i == cells[i].i);
+    }
+
+    space_destroy(space);
+    return 0;
+}
+
+static const char *test_space_set_get_large_origin() {
+    static const uint64_t M = (1<<14);   // create M unique values..
+    static const uint64_t N = (1<<18)-1; // ..ranging from (0..N)
+    cell_t cells[M];
+    cell_t cell;
+    vector3_t vectors[M];
+    space_t *space = NULL;
+    uint64_t i;
+
+    space = space_create(vector3(0x100,0x100,4),M>>4);
+    // Set lots of cells near the origin, using the Knuth method to generate
+    // random unique numbers. See https://stackoverflow.com/a/1608585
+    // This tests the block access
+    uint64_t in, im, unique[M], rn, rm;
+    im = 0;
+    for (in = 0; in < N && im < M; ++in) {
+        rn = N - in;
+        rm = M - im;
+        if ((uint64_t)rand() % rn < rm)
+        unique[im++] = in;
+    }
+    // unique is now an M-long vector of random, _unique_ numbers, 18 bits long
+    // we generate vectors by splitting it as [17:16][15:8][7:0] => zyx
+    for (i = 0; i < M; ++i) {
+        cells[i].i = rand();
+        vectors[i] = vector3(unique[i] & 0xff, (unique[i]>>8) & 0xff, (unique[i]>>16) & 0x3);
+        space_set(space, vectors[i], cells[i]);
+    }
+    // Check cells
+    for (i = 0; i < M; ++i) {
+        cell = space_get(space, vectors[i]);
+        mu_assert(_FL "bad cell value", cell.i == cells[i].i);
+    }
+
+    space_destroy(space);
     return 0;
 }
 
 static const char *all_tests() {
     mu_run_test(test_space_create_destroy);
-    mu_run_test(test_space_set_get_small);
-    //mu_run_test(test_space_set_get_large);
+    mu_run_test(test_space_set_get_large);
+    mu_run_test(test_space_set_get_large_origin);
     return 0;
 }
 
